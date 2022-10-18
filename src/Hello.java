341c331b558580c1969fd53be686a849fbb8dd4c
barcelona <mohit_nitd@outlook.com>
2022-10-18 01:39:17 -0500

    basic math

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,17 @@
 public class Hello {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-            System.out.println("Hello World!");
-            int myFirstNumber = 5;
-        System.out.println("myFirstNumber" + myFirstNumber);
+        System.out.println("Hello World!");
+        int myFirstNumber = 35;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("myTotal" + " " + myTotal);
+        int mylastOne = 1000 - myTotal;
+        System.out.println(mylastOne);
+
 
     }
 }
