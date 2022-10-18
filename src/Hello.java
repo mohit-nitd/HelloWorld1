b50ec831e1352f1d25173890d38384b5730dd6fc
barcelona <mohit_nitd@outlook.com>
2022-10-18 01:05:43 -0500

    variables

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,9 @@ public class Hello {
 
     public static void main(String[] args){
 
-            System.out.println("");
+            System.out.println("Hello World!");
+            int myFirstNumber = 5;
+        System.out.println("myFirstNumber" + myFirstNumber);
 
     }
 }
